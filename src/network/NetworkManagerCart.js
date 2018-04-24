bd355d394970d8ac01238a3d48ea560bf3e1a2ba
zhx <644544623@qq.com>
2018-04-24 17:56:26 +0800

    我的页面 购物车页面搞定

--- src/network/NetworkManagerCart.js
+++ src/network/NetworkManagerCart.js
@@ -2,14 +2,14 @@ import NetworkManager from "./NetworkManager";
 
 export default class NetworkManagerCart extends NetworkManager{
     static addCart(params) {
-        return this.instance().POST('/order/addToShoppingCart', params)
+        return this.instance().POST('/order/addToShoppingCart',  {'userId': 123, ...params}, {'Content-Type': 'application/json'})
     }
 
-    static fetchCartGoods(userId) {
-        return this.instance().POST('/order/listShoppingCart', userId)
+    static fetchCartGoods() {
+        return this.instance().POST('/order/listShoppingCart', {userId: 123})
     }
 
     static deleteCartGoods(params) {
-        return this.instance().POST('/order/deleteFromShoppingCart', params)
+        return this.instance().POST('/order/deleteFromShoppingCart', {'userId': 123, ...params}, {'Content-Type': 'application/json'})
     }
 }
