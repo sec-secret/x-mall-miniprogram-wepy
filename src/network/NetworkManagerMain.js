11bcda892dd829d4bf694806c2658220c4886283
zhx <644544623@qq.com>
2018-04-25 18:42:53 +0800

    首页完成100% 推荐页 100%
    todo:
    搜索页
    所有的Loading和错误处理
    还有关于onLoad和onShow的细节修改
    商品详情的图片的宽度问题
    物流页
    地址页的相关跳转优化 和删除功能
    购物车的删除功能
    
    所有的支付相关
    所有的登录

--- src/network/NetworkManagerMain.js
+++ src/network/NetworkManagerMain.js
@@ -4,4 +4,7 @@ export default class NetworkManagerMain extends NetworkManager{
     static main(){
         return this.instance().POST('/main', {});
     }
+  static mallRecommend(catId, page) {
+    return this.instance().POST('/main/randomGoods', {catId, page})
+  }
 }
