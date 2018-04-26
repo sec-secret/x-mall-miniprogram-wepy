58ed991c33643869355159cf6b44a59669f807a1
zhx <644544623@qq.com>
2018-04-26 13:27:05 +0800

    搜索页完成
    todo:
    所有的Loading和错误处理
    还有关于onLoad和onShow的细节修改
    商品详情的图片的宽度问题
    物流页
    地址页的相关跳转优化 和删除功能
    购物车的删除功能
    
    所有的支付相关
    所有的登录

---
+++ src/network/NetworkManagerOther.js
@@ -0,0 +1,15 @@
+import NetworkManager from "./NetworkManager";
+
+export default class NetworkManagerOther extends NetworkManager{
+  static mallSearch(req) {
+    return this.instance().POST('/search/searchEsGoodsByPage', req, {'Content-Type': 'application/json'})
+  }
+
+  static mallSuggestions(query) {
+    return this.instance().POST('/search/suggestions', {query})
+  }
+
+  static searchBySuggestions(req) {
+    return this.instance().POST('/search/searchBySuggestions', req, {'Content-Type': 'application/json'})
+  }
+}
