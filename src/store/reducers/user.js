759c264253e1393e1d906d7ba842300df061b23f
zhx <644544623@qq.com>
2018-04-26 19:12:51 +0800

    商品组件  订单组件 地址组件  购物车组件提出来 wxs提出 地址的相关跳转问题
    TODO：
    Loading和错误处理
    详情宽度
    购物车和地址的删除功能
    所有支付相关和登录相关

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -3,8 +3,6 @@ import { UPDATE_USER_INFO } from '../types/user'
 
 export default handleActions({
   [UPDATE_USER_INFO] (state, action) {
-    console.log(state);
-    console.log(action);
     return {
       ...state,
       userInfo: {...action.userInfo}
