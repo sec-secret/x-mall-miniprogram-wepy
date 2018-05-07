e80dff76e4710eb08f648d52c2886df56abee21c
zhx <644544623@qq.com>
2018-05-07 12:11:47 +0800

    商品详情优化成功
    TODO：
    详情宽度
    所有支付相关和登录相关

--- src/network/NetworkManagerAuth.js
+++ src/network/NetworkManagerAuth.js
@@ -2,7 +2,7 @@ import NetworkManager from "./NetworkManager";
 
 export default class JNetworkMallAuth extends NetworkManager{
   static getUserInfo(code){
-    return this.instance().freedomPOST('http://mall.beta.zookainet.com/gateway/auth/login/getUserInfo', {code});
+    return this.instance().freedomPOST('http://mall.beta.zookainet.com/gateway/auth/', 'login/getUserInfo', {code});
   }
 
   // static userLogin(mobile, verifyCode, verifyKey, openId, nickName) {
@@ -10,14 +10,14 @@ export default class JNetworkMallAuth extends NetworkManager{
   // }
 
   static userLogin(grant_type, username, password) {
-    return this.instance().freedomPOST('http://mall.beta.zookainet.com/gateway/auth/oauth/token', {grant_type, username, password});
+    return this.instance().freedomPOST('http://mall.beta.zookainet.com/gateway/auth/', 'oauth/token', {grant_type, username, password});
   }
   // grant_type      固定  password
   // username      传手机号
   // password       传openId
 
   static getVerifyCode(mobile, openId) {
-    return this.instance().freedomPOST('http://mall.beta.zookainet.com/gateway/auth/login/getVerifyCode', {mobile, openId})
+    return this.instance().freedomPOST('http://mall.beta.zookainet.com/gateway/auth/', 'login/getVerifyCode', {mobile, openId})
   }
 
   static miniprogramCode2Session(code){
