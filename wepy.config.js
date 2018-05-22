9b5504ec10d8d36c9013ee5c6cd06e1aba145e75
cuppi <cuppi413@163.com>
2018-05-22 18:08:43 +0800

    第一个table版本

--- wepy.config.js
+++ wepy.config.js
@@ -41,6 +41,7 @@ module.exports = {
         'transform-decorators-legacy',
         'transform-object-rest-spread',
         'transform-export-extensions',
+        'syntax-export-extensions'
       ]
     }
   },
